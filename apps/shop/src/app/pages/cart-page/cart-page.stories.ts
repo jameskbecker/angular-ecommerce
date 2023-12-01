cf09dcc04c4fa55411d528cd8f93679406ea77cf
James Becker <work@jameskbecker.com>
2023-12-01 16:25:48 +0100

    remove browser module

--- apps/shop/src/app/pages/cart-page/cart-page.stories.ts
+++ apps/shop/src/app/pages/cart-page/cart-page.stories.ts
@@ -3,14 +3,10 @@ import { moduleMetadata } from '@storybook/angular';
 import { CartPageComponent } from './cart-page.component';
 import { FeaturesModule } from '../../features/features.module';
 import { UiModule } from '../../ui/ui.module';
-import { CartService } from '../../services/cart.service';
-import { PagesModule } from '../pages.module';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslocoDirective } from '@ngneat/transloco';
 import { IconsModule } from '../../icons/icons.module';
-import { RouterLink } from '@angular/router';
 
 const meta: Meta<CartPageComponent> = {
   component: CartPageComponent,
@@ -21,7 +17,6 @@ const meta: Meta<CartPageComponent> = {
         UiModule,
         CommonModule,
         UiModule,
-        BrowserModule,
         NgOptimizedImage,
         HttpClientModule,
         FeaturesModule,
